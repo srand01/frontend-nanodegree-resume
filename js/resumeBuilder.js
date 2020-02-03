6ab94e283dbef349c0dd8d26ab2a62a1fd08cdd7
ra <44007192+srand01@users.noreply.github.com>
2020-02-03 16:25:47 -0400

    fixed typo and change order

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var formattedRole = HTMLheaderRole.replace("%data%", "Web developer");
+$("#header").prepend(formattedRole);
+
 var formattedName = HTMLheaderName.replace("%data%", "srand01");
 $("#header").prepend(formattedName);
-
-var formattedRole = HTMLheaderRole.replace("%data%", "Web developer");
-$("header").prepend(formattedRole);
