e8bb98e0d45a4ada23c75385995ab567a9539834
ra <44007192+srand01@users.noreply.github.com>
2020-02-11 22:33:07 -0400

    added bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,3 +6,20 @@ $("#header").prepend(formattedRole);
 
 var formattedName = HTMLheaderName.replace("%data%", "srand01");
 $("#header").prepend(formattedName);
+
+var bio = {
+    "name": "srand01",
+    "role": "Web Developer",
+    "contacts": {
+        "mobile": "000-000-0000",
+        "email": "srand01 github",
+        "github": "srand01",
+        "twitter": "N/A",
+        "location": "earth"
+    },
+    "WelcomeMessage": "Hi there!",
+    "skills": ["HTML", "CSS", "JAVASCRIPT"],
+    "bioPic": "chairmander.png"
+
+
+};
